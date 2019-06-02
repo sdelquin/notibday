864bf04c1e0a8f28d8d23f14431233cea6ac0395
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-06-02 20:05:16 +0100

    Add module for retrieve and query Google Contacts

---
+++ gcontacts.py
@@ -0,0 +1,39 @@
+import datetime
+
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import client, file, tools
+
+import config
+
+
+class GContacts:
+    def __init__(self):
+        store = file.Storage('token.json')
+        creds = store.get()
+        if not creds or creds.invalid:
+            flow = client.flow_from_clientsecrets('credentials.json',
+                                                  config.GOOGLE_API_SCOPES)
+            creds = tools.run_flow(flow, store)
+        self.service = build('people', 'v1', http=creds.authorize(Http()))
+
+    def retrieve_contacts(self):
+        data = self.service.people().connections().list(
+            resourceName='people/me',
+            pageSize=2000,
+            personFields='names,birthdays,emailAddresses').execute()
+
+        self.contacts = {}
+        for contact in data['connections']:
+            if 'names' in contact.keys():
+                full_name = contact['names'][0]['displayName']
+                if 'birthdays' in contact.keys():
+                    birthday = contact['birthdays'][0]['date']
+                    self.contacts[full_name] = datetime.date(**birthday)
+
+    def get_contact_age(self, contact_name):
+        birthdate = self.contacts.get(contact_name)
+        if birthdate is None:
+            return -1
+        delta = datetime.date.today() - birthdate
+        return round(delta.days / 365)
